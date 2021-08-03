b81144665d9192544d45bcfe93601e299a141456
emreicoz <emreicoz1905@gmail.com>
2021-08-03 16:46:55 +0300

    Project created

---
+++ src/main/java/com/example/springbootwebmarketingapp/SpringBootWebMarketingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebmarketingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebMarketingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebMarketingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwebmarketingapp/SpringBootWebMarketingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebmarketingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebMarketingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
